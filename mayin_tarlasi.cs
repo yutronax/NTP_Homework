cb6be22d10e0a15e49b5a73116275eb1f1a1a559
Elyasa153 <88188061+Elyasa153@users.noreply.github.com>
2025-06-10 14:46:47 +0300

    Update mayin_tarlasi.cs

--- mayin_tarlasi.cs
+++ mayin_tarlasi.cs
@@ -1,168 +1,232 @@
 using minesweeper;
 using System.Collections;
 using System.Drawing.Text;
+using System.IO.Compression;
 using System.Web;
 using System.Windows.Forms;
 
 namespace minesweeper
-{   
-    public class Hucre2
+{
+    public partial class Form1 : Form
     {
-        List<int> acildiMi=new List<int>();
-        bool mayınlarsecildimi = false;
-        private Button btn;
-        Button Btn { get { return btn; } set { btn = value; } }
-        FlowLayoutPanel panel = new FlowLayoutPanel();
+        public Form1()
+        {
+            InitializeComponent();
+            this.MaximizeBox = false;
+        }
 
-        public List<int> button_saylari;
-        public void butonlar(FlowLayoutPanel f)
-        {Button ilk = new Button();
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            flowLayoutPanel1.FlowDirection = FlowDirection.LeftToRight;
+            flowLayoutPanel1.WrapContents = true;
+            zorluk z = new zorluk();
+            z.zorlukbutonlar(flowLayoutPanel1);
+        }
+    }
+
+    public class zorluk
+    {
+        string[] dizi = { "kolay", "orta", "zor" };
+        public int h = 0;
+        FlowLayoutPanel panel;
+
+        public void zorlukbutonlar(FlowLayoutPanel f)
+        {
             panel = f;
-            for (int i = 1; i < 91; i++)
+
+            for (int i = 0; i < 3; i++)
+            {
+                Button btn = new Button();
+                btn.Text = dizi[i];
+                btn.Size = new Size(300, 300);
+                f.Controls.Add(btn);
+                btn.Click += zorlukbutontiklandi;
+            }
+        }
+
+        public void zorlukbutontiklandi(object sender, EventArgs e)
+        {
+            Button tiklanan = sender as Button;
+            for (int i = 0; i < 3; i++)
+            {
+                if (tiklanan.Text.Equals(dizi[i]))
+                {
+                    h = i;
+                    panel.Controls.Clear();
+                    Tahta t = new Tahta();
+                    t.degerdondur(h);
+                    t.butonlar(panel);
+                    return;
+                }
+            }
+        }
+    }
+
+    public class Tahta
+    {
+        protected FlowLayoutPanel panel = new FlowLayoutPanel();
+        private Button btn;
+        bool yerlestirme;
+        protected int deger;
+        protected int hucre_sayisi = 100;
+        Button Btn { get { return btn; } set { btn = value; } }
+        private Button bayrakbtn;
+
+        int[] grid_genislikleri = { 8, 10, 15 }; 
+        public int grid_genisligi;
+
+        public Button Bayrakbtn
+        {
+            get { return bayrakbtn; }
+            set { bayrakbtn = value; }
+        }
+
+        public void degerdondur(int h)
+        {
+            deger = h;
+        }
+
+        public void butonlar(FlowLayoutPanel f)
+        {
+            panel = f;
+            int boyut = 120 - (deger * 30);
+
+            grid_genisligi = grid_genislikleri[deger];
+            hucre_sayisi = 9 * grid_genisligi; 
+
+            Hucre h = new Hucre(f, deger, grid_genisligi);
+
+            for (int i = 1; i <= hucre_sayisi; i++)
             {
                 Btn = new Button();
                 Btn.Text = $"";
                 Btn.Name = $"Btn{i}";
-                Btn.Size = new Size(90, 90);
+                Btn.Size = new Size(boyut, 90);
                 panel.Controls.Add(Btn);
-                Btn.Click += buttonatıklandı;
+                Btn.Click += h.buttonatıklandı;
+                Btn.MouseDown += h.bayrak; 
             }
         }
+    }
+
+    class Hucre : Tahta
+    {
+        List<int> acildiMi = new List<int>();
+        bool mayınlarsecildimi = false;
+        public FlowLayoutPanel gamePanel;
+        public List<int> button_saylari;
+        zorluk z = new zorluk();
+        private int toplam_hucre; 
+
+        public Hucre(FlowLayoutPanel panel, int zorlukDegeri, int gridGenisligi)
+        {
+            gamePanel = panel;
+            this.deger = zorlukDegeri;
+            this.grid_genisligi = gridGenisligi;
+            this.toplam_hucre = 9 * gridGenisligi;
+        }
+
+        public void bayrak(object sender, MouseEventArgs e)
+        {
+            Button bayrakBtn = sender as Button;
+            if (e.Button == MouseButtons.Right)
+            {
+                if (bayrakBtn.Text == "")
+                {
+                    bayrakBtn.Text = "🚩";
+                    bayrakBtn.Tag = "bayrak";
+                }
+                else if (bayrakBtn.Text == "🚩")
+                {
+                    bayrakBtn.Text = "";
+                    bayrakBtn.Tag = null;
+                }
+            }
+        }
+
         void sifirlarlistele(int sayi)
         {
-
             if (acildiMi.Contains(sayi))
             {
                 return;
             }
-            mayinanla(sayi);
 
-            if (sayi < 10 && sayi > 1)
+            int mayinSayisi = mayinanla(sayi);
+            List<int> komsular = komsugetir(sayi);
+
+            if (mayinSayisi != 0)
             {
-               if  (mayinanla(sayi + 1)==0) sifirlarlistele(sayi+1);
-                if (mayinanla(sayi - 1) == 0) sifirlarlistele(sayi - 1);
-                if (mayinanla(sayi + 9) == 0) sifirlarlistele(sayi + 9);
-                if (mayinanla(sayi + 10) == 0) sifirlarlistele(sayi + 10);
-                if (mayinanla(sayi + 11) == 0) sifirlarlistele(sayi + 11);
-
-
-            }
-            else if (sayi > 81 && sayi < 90)
-            {
-                if (mayinanla(sayi + 1) == 0) sifirlarlistele(sayi + 1);
-                if (mayinanla(sayi - 1) == 0) sifirlarlistele(sayi - 1);
-                if (mayinanla(sayi - 9) == 0) sifirlarlistele(sayi - 9);
-                if (mayinanla(sayi - 10) == 0) sifirlarlistele(sayi - 10);
-                if (mayinanla(sayi - 11) == 0) sifirlarlistele(sayi - 11);
-
-
+                return;
             }
 
-            else if (sayi == 1)
+            foreach (int komsu in komsular)
             {
-                if (mayinanla(sayi + 1) == 0) sifirlarlistele(sayi +1);
-                if (mayinanla(sayi + 11) == 0) sifirlarlistele(sayi + 11);
-                if (mayinanla(sayi + 10) == 0) sifirlarlistele(sayi + 10);
-
-
-
-
-            }
-            else if (sayi == 10)
-            {
-                if (mayinanla(sayi + 10) == 0) sifirlarlistele(sayi + 10);
-                
-                    if (mayinanla(sayi - 1) == 0) sifirlarlistele(sayi - 1);
-                    if (mayinanla(sayi + 9) == 0) sifirlarlistele(sayi + 9);
-
+                if (!acildiMi.Contains(komsu))
+                {
+                    sifirlarlistele(komsu);
                 }
-          
-            else if (sayi == 81)
-            {
-                if (mayinanla(sayi + 1) == 0) sifirlarlistele(sayi + 1);
-                if (mayinanla(sayi - 9) == 0) sifirlarlistele(sayi - 9);
-                if (mayinanla(sayi + 10) == 0) sifirlarlistele(sayi - 10);
-
-
-
-            }
-            else if (sayi == 90)
-            {
-                if (mayinanla(sayi - 11) == 0) sifirlarlistele(sayi - 11);
-                if (mayinanla(sayi - 1) == 0) sifirlarlistele(sayi - 1);
-
-                if (mayinanla(sayi + 10) == 0) sifirlarlistele(sayi - 10);
-
-
-
-            }
-            else if (sayi % 10 == 1)
-            {
-                if (mayinanla(sayi + 1) == 0) sifirlarlistele(sayi + 1);
-                if (mayinanla(sayi - 9) == 0) sifirlarlistele(sayi - 9);
-                if (mayinanla(sayi + 11) == 0) sifirlarlistele(sayi + 11);
-
-
-                if (mayinanla(sayi + 10) == 0) sifirlarlistele(sayi + 10);
-                if (mayinanla(sayi - 10) == 0) sifirlarlistele(sayi - 10);
-
-
-            }
-            else if (sayi % 10 == 0)
-            {
-                if (mayinanla(sayi - 1) == 0) sifirlarlistele(sayi - 1);
-                if (mayinanla(sayi + 9) == 0) sifirlarlistele(sayi + 9);
-                if (mayinanla(sayi - 11) == 0) sifirlarlistele(sayi - 11);
-
-
-                if (mayinanla(sayi + 10) == 0) sifirlarlistele(sayi + 10);
-                if (mayinanla(sayi - 10) == 0) sifirlarlistele(sayi - 10);
-
-
-            }
-            else
-            {
-                if (mayinanla(sayi + 1) == 0) sifirlarlistele(sayi + 1);
-                if (mayinanla(sayi - 1) == 0) sifirlarlistele(sayi - 1);
-                if (mayinanla(sayi - 9) == 0) sifirlarlistele(sayi - 9);
-                if (mayinanla(sayi - 10) == 0) sifirlarlistele(sayi - 10);
-                if (mayinanla(sayi - 11) == 0) sifirlarlistele(sayi - 11);
-                if (mayinanla(sayi + 9) == 0) sifirlarlistele(sayi + 9);
-                if (mayinanla(sayi + 10) == 0) sifirlarlistele(sayi + 10);
-                if (mayinanla(sayi + 11) == 0) sifirlarlistele(sayi + 11);
             }
         }
+
+   
+        private List<int> komsugetir(int sayi)
+        {
+            List<int> komsular = new List<int>();
+            int satir = (sayi - 1) / grid_genisligi; 
+            int sutun = (sayi - 1) % grid_genisligi; 
+
+            for (int ilk = -1; ilk <= 1; ilk++)
+            {
+                for (int son = -1; son <= 1; son++)
+                {
+                    if (ilk == 0 && son == 0) continue; 
+
+                    int yeniSatir = satir + ilk;
+                    int yeniSutun = sutun + son;
+
+                    if (yeniSatir >= 0 && yeniSatir < 9 &&
+                        yeniSutun >= 0 && yeniSutun < grid_genisligi)
+                    {
+                        int yeniSayi = yeniSatir * grid_genisligi + yeniSutun + 1;
+                        komsular.Add(yeniSayi);
+                    }
+                }
+            }
+            return komsular;
+        }
+
         public void buttonatıklandı(object sender, EventArgs e)
         {
             Button tıklanan = sender as Button;
+            if (tıklanan.Tag != null && tıklanan.Tag.ToString() == "bayrak")
+            {
+                return; 
+            }
+
             string butonismi = tıklanan.Name;
             int sayi = int.Parse(string.Join("", tıklanan.Name.Where(char.IsDigit).Select(c => c.ToString())));
 
-
             if (mayınlarsecildimi == false)
             {
-            
-
                 button_saylari = mayinsec(sayi);
                 mayınlarsecildimi = true;
             }
-            
+
             if (tıklanan != null)
             {
                 oyunbitti(tıklanan);
                 sifirlarlistele(sayi);
-
             }
-
-
         }
+
         private void oyunbitti(object sender)
         {
             Button tıklanan = sender as Button;
             int sayi = int.Parse(string.Join("", tıklanan.Name.Where(char.IsDigit).Select(c => c.ToString())));
+
             if (button_saylari.Contains(sayi))
             {
-                foreach(int item in button_saylari)
+                foreach (int item in button_saylari)
                 {
                     string butonAdi = $"Btn{item}";
                     Control[] bulunan = tıklanan.Parent.Controls.Find(butonAdi, false);
@@ -174,213 +238,86 @@ namespace minesweeper
                 MessageBox.Show("OYUN BİTTİ!");
                 Application.Exit();
             }
-           
-
         }
-        
+
         private int tarama(int say)
         {
-            int sayac = 0;
             foreach (int i in button_saylari)
             {
                 if (i == say)
                 {
-                    sayac++;
-                    return sayac;
+                    return 1; 
                 }
             }
             return 0;
-
         }
 
         private int mayinanla(int sayi)
         {
             if (button_saylari.Contains(sayi))
             {
-                return -1;
-
+                return -1; 
             }
 
+            List<int> komsular = komsugetir(sayi);
             int mayin_sayisi = 0;
-            if (sayi < 10 && sayi > 1)
+
+            foreach (int komsu in komsular)
             {
-                mayin_sayisi += tarama(sayi + 1);
-                mayin_sayisi += tarama(sayi - 1);
-                mayin_sayisi += tarama(sayi + 10);
-                mayin_sayisi += tarama(sayi + 9);
-                mayin_sayisi += tarama(sayi + 11);
-
-
+                if (button_saylari.Contains(komsu))
+                {
+                    mayin_sayisi++;
+                }
             }
-            else if (sayi > 81 && sayi < 90)
-            {
-                mayin_sayisi += tarama(sayi + 1);
-                mayin_sayisi += tarama(sayi - 1);
-                mayin_sayisi += tarama(sayi - 10);
-                mayin_sayisi += tarama(sayi - 9);
-                mayin_sayisi += tarama(sayi - 11);
 
-            }
-            else if (sayi % 10 == 0)
+            foreach (Control control in gamePanel.Controls)
             {
-                mayin_sayisi += tarama(sayi - 1);
-                mayin_sayisi += tarama(sayi - 10);
-                mayin_sayisi += tarama(sayi + 9);
-                mayin_sayisi += tarama(sayi - 11);
-                mayin_sayisi += tarama(sayi + 10);
-
-            }
-            else if (sayi % 10 == 1)
-            {
-
-                mayin_sayisi += tarama(sayi + 1);
-                mayin_sayisi += tarama(sayi - 10);
-                mayin_sayisi += tarama(sayi - 9);
-                mayin_sayisi += tarama(sayi + 11);
-                mayin_sayisi += tarama(sayi + 10);
-            }
-            else if (sayi == 1)
-            {
-                mayin_sayisi += tarama(sayi + 1);
-                mayin_sayisi += tarama(sayi + 10);
-                mayin_sayisi += tarama(sayi + 11);
-
-            }
-            else if (sayi == 10)
-            {
-                mayin_sayisi += tarama(sayi - 1);
-                mayin_sayisi += tarama(sayi + 10);
-                mayin_sayisi += tarama(sayi + 9);
-
-            }
-            else if (sayi == 81)
-            {
-                mayin_sayisi += tarama(sayi - 10);
-                mayin_sayisi += tarama(sayi - 9);
-                mayin_sayisi += tarama(sayi + 1);
-
-            }
-            else if (sayi == 90)
-            {
-                mayin_sayisi += tarama(sayi - 10);
-                mayin_sayisi += tarama(sayi - 1);
-                mayin_sayisi += tarama(sayi - 9);
-
-            }
-            else
-            {
-
-                mayin_sayisi += tarama(sayi + 1);
-                mayin_sayisi += tarama(sayi - 1);
-                mayin_sayisi += tarama(sayi - 10);
-                mayin_sayisi += tarama(sayi + 10);
-                mayin_sayisi += tarama(sayi - 11);
-                mayin_sayisi += tarama(sayi + 11);
-                mayin_sayisi += tarama(sayi - 9);
-                mayin_sayisi += tarama(sayi + 9);
-
-            }
-            foreach (Control tıklanan in panel.Controls)
-            {
-                int sayi2 = int.Parse(string.Join("", tıklanan.Name.Where(char.IsDigit).Select(c => c.ToString())));
+                int sayi2 = int.Parse(string.Join("", control.Name.Where(char.IsDigit).Select(c => c.ToString())));
                 if (sayi2 == sayi)
                 {
-                    tıklanan.Text = $"{mayin_sayisi}";
-                    if (mayin_sayisi == 1)
+                    if (mayin_sayisi > 0)
                     {
-                        tıklanan.BackColor = Color.Blue;
+                        control.Text = $"{mayin_sayisi}";
+                        // Renk ayarlaması
+                        if (mayin_sayisi == 1) control.BackColor = Color.Blue;
+                        else if (mayin_sayisi == 2) control.BackColor = Color.Green;
+                        else if (mayin_sayisi == 3) control.BackColor = Color.Brown;
+                        else if (mayin_sayisi == 4) control.BackColor = Color.Purple;
+                        else if (mayin_sayisi == 5) control.BackColor = Color.Orange;
+                        else if (mayin_sayisi == 6) control.BackColor = Color.Pink;
                     }
-                    else if (mayin_sayisi == 2)
+                    else
                     {
-                        tıklanan.BackColor = Color.Green;
-
+                        control.BackColor = Color.LightGray;
                     }
-                    else if (mayin_sayisi == 3)
-                    {
-                        tıklanan.BackColor = Color.Brown;
-
-                    }
-                    else if (mayin_sayisi == 4)
-                    {
-                        tıklanan.BackColor = Color.Purple;
-
-                    }
-                    else if (mayin_sayisi == 5)
-                    {
-                        tıklanan.BackColor = Color.Orange;
-
-                    }
-                    else if (mayin_sayisi == 6)
-                    {
-                        tıklanan.BackColor = Color.Pink;
-
-                    }
+                    break;
                 }
-
             }
+
             acildiMi.Add(sayi);
             return mayin_sayisi;
         }
+
         public List<int> mayinsec(int x)
         {
+            int mayinalgo = 10 + (20 * this.deger);
             Random r = new Random();
-
             List<int> mayinliste = new List<int>();
-            int mayin = 0;
-            bool varmi = false;
-            for (int i = 0; i < 30; i++)
-            {
-                varmi = false;
-                mayin = r.Next(0, 91);
-                if (mayinliste.Count != 0)
-                {
-                    foreach (int j in mayinliste)
-                    {
-                        if (j == mayin || x==mayin)
-                        {
-                            varmi = true;
-                            i--;
-                            break;
-                        }
 
-                    }
-                }
-                if (varmi == false)
+            List<int> korunanlar = new List<int> { x };
+            korunanlar.AddRange(komsugetir(x));
+
+            while (mayinliste.Count < mayinalgo)
+            {
+                int mayin = r.Next(1, toplam_hucre + 1);
+
+                if (!mayinliste.Contains(mayin) && !korunanlar.Contains(mayin))
                 {
                     mayinliste.Add(mayin);
                 }
-
-
-
             }
+
             return mayinliste;
-        }
-
-    }
-
-
-    public partial class Form1 : Form
-    {
-
-        
-        public Form1()
-        {
-            InitializeComponent();
-            this.MaximizeBox = false;
-
-
-        }
-
-
-
-        private void Form1_Load(object sender, EventArgs e)
-        {
-            flowLayoutPanel1.FlowDirection = FlowDirection.LeftToRight;
-            flowLayoutPanel1.WrapContents = true;
-            
-            Hucre2 h = new Hucre2();
-            h.butonlar(flowLayoutPanel1);
-            
         }
     }
 }
